93b028555de6a4b03d3b21a34256e2eb674865c7
Ellie <eagyeman1234@gmail.com>
2023-09-05 09:54:24 -0400

    add init function

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
+import { getRandomFunFactOcean } from "./fun-facts.js"
+
 /*-------------------------------- Constants --------------------------------*/
+
+
 const categoryChoices = ['ocean', 'birds', 'fish']
 
 
@@ -22,31 +26,58 @@ const oceanButton = document.getElementById('oceans')
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-oceanButton.addEventListener('click', function(e) {
-  console.log('The button was clicked')
-})
+// oceanButton.addEventListener('click', function (e) {
+//    const funFact = prompt(funFactOceans)
+
+// })
+oceanButton.addEventListener('click', createFunFactOcean)
+
+// oceanButton.addEventListener('click', createFunFactOcean)
+console.log('the button is clicked')
 // document.getElementById('birds').addEventListener('onClick',play)
 // document.getElementById('fish').addEventListener('onClick',play)
 
 
 /*-------------------------------- Functions --------------------------------*/
 
-// function play(event){
-//   console.log('The button was clicked')
-// }
+
+function init() {
+   playerOne()
+   playerTwo()
+   turn = 1
+   winner = false
+   tie = false
+
+}
 
 
 
 
 
 
-// function play(event) {
-// getPlayerOne(event)
-// }
 
 
-// function getPlayerOne(event) {
-//   playerOne = event.target.id
-// }
 
-// console.log(getPlayerOne)
+
+function createFunFactOcean() {
+
+   const newFunFact = getRandomFunFactOcean()
+   funFactOceans.push(newFunFact)
+   render()
+}
+
+function render() {
+   createFunFactOcean()
+}
+
+
+
+
+
+
+
+
+
+
+
+
--- js/fun-facts.js
+++ js/fun-facts.js
@@ -7,21 +7,22 @@ const funFactOceans = [
   { isOceans: true, fact: 'About 70 percent of the oxygen we breathe	is produced by the oceans.' },
   { isOceans: true, fact: 'The smallest ocean is the Arctic Ocean.' },
   { isOceans: true, fact: 'Explorers of the sea and its mysteries are sometimes called oceanauts.' },
-  { isOceans: true, fact: 'While the words ocean and sea are used interchangeably, a sea is technically a smaller body of salt water that is part of a larger ocean.' }
-
-// isOceans: false fact: ‘The ocean covers 60 percent of the Earth’s surface.’
-//   isOceans: false fact: ‘The deepest part of the ocean is called The Ultimate Deep.’
-//   isOceans: false fact: ‘Less than 10 percent of the has been explored.’
-//   isOceans: false fact: ‘The ocean is blue because water absorbs colors in the blue part of the light spectrum.
-//     isOceans: false fact: ‘The ocean floor does not move.’
-//   isOceans: false fact: ‘The ocean is thousands of years old.’
-//   isOceans: false fact: ’Alligators live in the ocean and prey on fish.’
-//   isOceans: false fact: ‘The Bermuda Triangle is located in a section of the South Atlantic Ocean.
-
-
-
-
-
-
+  { isOceans: true, fact: 'While the words ocean and sea are used interchangeably, a sea is technically a smaller body of salt water that is part of a larger ocean.' },
+  { isOceans: false, fact: 'The ocean covers 60 percent of the Earths surface.' },
+  { isOceans: false, fact: 'The deepest part of the ocean is called The Ultimate Deep.' },
+  { isOceans: false, fact: 'Less than 10 percent of the has been explored.' },
+  { isOceans: false, fact: 'The ocean is blue because water absorbs colors in the blue part of the light spectrum.' },
+  { isOceans: false, fact: 'The ocean floor does not move.' },
+  { isOceans: false, fact: 'The ocean is thousands of years old.' },
+  { isOceans: false, fact: 'Alligators live in the ocean and prey on fish.' },
+  { isOceans: false, fact: 'The Bermuda Triangle is located in a section of the South Atlantic Ocean.' }
 
 ]
+
+function getRandomFunFactOcean() {
+  return funFactOceans[Math.floor(Math.random() * getRandomFunFactOcean.length)]
+}
+
+export {
+  getRandomFunFactOcean,
+}
