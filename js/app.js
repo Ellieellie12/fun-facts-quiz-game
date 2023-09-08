4a0f1e0129fc6f6cff3897bae1a34f1d78efd7ea
Ellie <eagyeman1234@gmail.com>
2023-09-08 09:24:28 -0400

    add countdown timer

--- js/app.js
+++ js/app.js
@@ -6,40 +6,33 @@ import { funFactsBirds } from "./fun-facts.js"
 /*-------------------------------- Constants --------------------------------*/
 const categoryChoices = ['ocean', 'birds', 'fish']
 const currentCategoryFacts = []
-// let currentCategoryIndex = 0
+
 /*---------------------------- Variables (state) ----------------------------*/
 let currentCategory, winner, randomIndex
 let score = 0
 let currentCategoryIndex = 0
+let timeLeft = 40
 /*------------------------ Cached Element References ------------------------*/
 const scoreElement = document.getElementById('score')
 const backgroundMusic = document.getElementById('gamemusic')
-
+const countDownEl =document.getElementById('countdown')
 const oceanButton = document.getElementById('oceans')
 const birdsButton = document.getElementById('birds')
 const fishButton = document.getElementById('fish')
-
-
 const resetButton = document.getElementById('resetbutton')
-
 const messageResult = document.getElementById('messageEl')
-
 const trueButton = document.getElementById('trueocean')
 const falseButton = document.getElementById('falseocean')
-
 const trueButtonBird = document.getElementById('truebird')
 const falseButtonBird = document.getElementById('falsebird')
 
 const trueButtonFish = document.getElementById('truefish')
 const falseButtonFish = document.getElementById('falsefish')
-// const trueAndFalseSections = document.querySelector('.trueandfalseoceans')
-// console.log(trueAndFalseSections)
-//NEED TO MAKE TRUE AND FALSE BUTTONS FOR BIRDS AND FISH SO THAT I CAN KEEP TRACK OF THEIR SCORE
+
 const oceanImage = document.getElementById('oceansimg')
 const birdsImage = document.getElementById('birdsimg')
 const fishImage = document.getElementById('fishimg')
 
-// const nextFactDiv = document.getElementById('factEl')
 /*----------------------------- Event Listeners -----------------------------*/
 trueButton.addEventListener('click', () => {
    checkAnswerOcean(true)
@@ -48,41 +41,19 @@ falseButton.addEventListener('click', () => {
    checkAnswerOceanTwo(notTrue)
 })
 
-// trueButton.addEventListener('click', function() {
-//    checkAnswerOcean(true)
-//    score = score + 1
-//    console.log(score)
-// })
-
-// falseButton.addEventListener('click', function() {
-   
-// })
-
-
-
 oceanButton.addEventListener('click', createFunFactOcean)
 birdsButton.addEventListener('click', createFunFactBird)
 fishButton.addEventListener('click', createFunFactFish)
-
 resetButton.addEventListener('click', init)
-
 document.getElementById('trueocean').addEventListener('click', nextQuestionOne)
 document.getElementById('falseocean').addEventListener('click', nextQuestionOcean)
-
 document.getElementById('truebird').addEventListener('click', nextQuestionTwo)
 document.getElementById('falsebird').addEventListener('click', nextQuestionBirds)
-
 document.getElementById('truefish').addEventListener('click', nextQuestionThree)
 document.getElementById('falsefish').addEventListener('click', nextQuestionFish)
 
-// oceanImage.addEventListener('click', function (evt) { 
-//   oceanSays.volume = 0.5
-//   oceanSays.play()
-// })
 /*-------------------------------- Functions --------------------------------*/
 function init() {
-   // clearEverything()
-
    winner = false
    score = 0
    messageResult.innerHTML = ''
@@ -96,24 +67,23 @@ function init() {
    trueButtonFish.style.display = 'none'
    falseButtonFish.style.display = 'none'
    playBackgroundMusic()
-   // createFunFactOcean()
    updateScore()
    render()
 }
 
 function playBackgroundMusic() {
-backgroundMusic.play() 
+   backgroundMusic.play()
 }
 
+let timer = setInterval(function(){
+   countDownEl.textContent = timeLeft + ' seconds remaining '
+   timeLeft -= 1
+   if(timeLeft < 0){
+      countDownEl.textContent = 'Game Over!!!'
+      clearInterval(timer)
+      }
 
-// setTimeout(function() {
-//    backgroundMusic.play
-//    backgroundMusic.pause()
-// }, 3000)
-
-
-
-
+   },1000)
 
 function createFunFactOcean() {
    oceanImage.style.visibility = 'hidden'
@@ -122,7 +92,6 @@ function createFunFactOcean() {
    messageResult.innerHTML = currentCategoryFacts[0].fact
    trueButton.style.display = 'block'
    falseButton.style.display = 'block'
-   
 
 }
 
@@ -133,6 +102,7 @@ function createFunFactBird() {
    messageResult.innerHTML = currentCategoryFacts[0].fact
    trueButtonBird.style.display = 'block'
    falseButtonBird.style.display = 'block'
+
 }
 
 function createFunFactFish() {
@@ -142,6 +112,7 @@ function createFunFactFish() {
    messageResult.innerHTML = currentCategoryFacts[0].fact
    trueButtonFish.style.display = 'block'
    falseButtonFish.style.display = 'block'
+
 }
 
 function nextQuestionOne() {
@@ -162,6 +133,7 @@ function nextQuestionTwo() {
    const random = randomFact()
    const birdFact = funFactsBirds[random]
    messageResult.innerHTML = birdFact.fact
+
 }
 
 function nextQuestionBirds() {
@@ -175,12 +147,14 @@ function nextQuestionThree() {
    const random = randomFact()
    const fishFact = funFactsFish[random]
    messageResult.innerHTML = fishFact.fact
+
 }
 
 function nextQuestionFish() {
    const random = randomFact()
    const fishFact = funFactsFish[random]
    messageResult.innerHTML = fishFact.fact
+
 }
 
 function randomFact() {
@@ -198,160 +172,19 @@ function checkAnswerOcean(isTrue) {
       updateScore()
    }
 }
-// messageResult.textContent = 'the'
+
 function clearEverything() {
    score = 0
    messageResult.textContent = ''
 
 }
 
-// function checkOceans() {
-
-// }
-
-
-
-function checkAnswerOceanTwo(isFalse) {
-   if (currentCategoryIndex < funFactsOceans.length) {
-      const wrongAnswer = funFactsOceans[currentCategoryIndex].isOceans
-      if (isFalse === wrongAnswer) {
-        -- score
-   }
-   currentCategoryIndex--
-   updateScore()
-
-  }
-}
-
 function updateScore() {
    scoreElement.innerHTML = score
 }
 
-// function filterOutDuplicates(currentFact) {
-//    return funFactsOceans.filter((fact) => fact.fact !== currentFact)
-// }
-// const duplicateElements = toF(funFactsOceans)
-
-
-
-function playerScore() {
-
-   //if else statement
-}
-
-
-
-
 function render() {
-   // playBackgroundMusic()
-   // createFunFactOcean()
-   // nextQuestionOne()
+   playBackgroundMusic()
+   randomFact()
+   checkAnswerOcean()
 }
-
-
-
-
-
-
-
-
-
-
-
-
-//NOTES FOR TOMORROW ADD A TIMER, SCORE BOARD, AUDIO, ASK ABOUT THE MESSAGE ELEMENT FOR BIRD AND FISH 
-
-
-
-
-
-
-
-
-
-// function nextQuestion() {
-//    if(currentCategoryIndex >= funFactsOceans.length) {
-//       nextFactDiv.innerHTML = 'No More Facts'
-//    } else {
-//       const newFact = funFactsOceans[currentCategoryIndex]
-//       nextFactDiv.innerHTML = currentCategoryFacts.fact
-//       currentCategoryIndex++
-//    }
-// }
-
-// function nextQuestion() {
-//    if(currentCategoryIndex < funFactsOceans.length) {
-//       const currentFact = funFactsOceans[currentCategoryIndex]
-//       messageResult.innerHTML = currentCategoryFacts.fact
-//       funFactsOceans.length++
-//    }
-// }
-
-// function nextQuestion(){
-//    const nextFact = Math.floor(Math.random() * funFactsOceans.length)
-//    currentCategory = funFactsOceans[nextFact]
-// }
-
-// function nextQuestion(){
-//    const trueButton = funFactsOceans[currentCategoryIndex]
-//    messageResult.innerHTML = currentCategoryFacts.fact
-//    currentCategoryIndex++
-//    if (currentCategoryFacts >= funFactsOceans.length) {
-//       currentCategoryIndex = 0
-
-//    }
-// }
-
-
-// function nextQuestion() {
-//    const answerTrue = funFactsOceans[0]
-//    currentCategoryFacts.push(isOceans)
-// }
-
-//   funFactsOceans.forEach() {
-//iterate through the array with forEach method?
-
-// funFactsOceans.push(newFunFact)
-
-// funFactsOceans.push(newFunFact)
-//then display one fun fact
-
-//i have to push my funfacts into my empty array of currentcategoryfacts
-// function nextQuestion(){
-//    const trueButton = funFactsOceans[currentCategoryIndex]
-//    messageResult.innerHTML = currentCategoryFacts.fact
-//    currentCategoryIndex++
-//    if (currentCategoryFacts >= funFactsOceans.length) {
-//       currentCategoryIndex = 0
-
-//    }
-// }
-
-
-// function nextQuestion() {
-//    const answerTrue = funFactsOceans[0]
-//    currentCategoryFacts.push(isOceans)
-// }
-
-// function nextQuestion() {
-//    const random = randomFact()
-//    const oceanFact = funFactsOceans[random]
-//    messageResult.innerHTML = oceanFact.fact
-//    //working code above just in case
-//    // if(currentCategoryFacts !== messageResult)
-//    // return 
-//    // messageResult.innerHTML = currentCategoryFacts.fact
-//    // for (let i = 0; i < funFactsOceans.length; i++) {
-//    //    randomIndex = randomFact()
-//    //    funFactsOceans.forEach((fact, index) => {
-//    //       if (fact[randomIndex] === funFactsOceans[index])
-//    //       funFactsOceans.pop()
-//    //          console.log(fact)
-//    //    })
-//    // }
-// }
-
-
-
-
-
